90ca8a2fb327d20718bea76a1229050b505ec842
Pulon Julien <pulonjulien@gmail.com>
2025-03-25 14:59:50 +0100

    add count in return of function print_number

--- print_number.c
+++ print_number.c
@@ -1,44 +1,40 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_number_rec - recursive function
  * @num: positive number
  */
 void print_number_rec(unsigned int num)
-{
+	{
 	if (num / 10)
 		print_number_rec(num / 10);
-
-	_putchar((num % 10) + '0');
-}
+		_putchar((num % 10) + '0');
+	}
 /**
  * print_number - print an integer  
  * @args: variable arguments
  * Return: number of char printed
- * 
  */
 int print_number(va_list args)
-{
+	{
 	int n = va_arg(args, int);
 	unsigned int num;
 	int count = 0;
 
 	if (n < 0)
-	{
-	
-	_putchar('-');
-	count++;
-	num = -n;
+		{
+		_putchar('-');
+		count++;
+		num = -n;
+		}
+	else 
+		{
+		num = n;
+		}
+	if (num / 10)
+		print_number_rec(num);
+		_putchar((num% 10) + '0');
+		count++;
+	return(count);
 	}
-
-	else (num = n);
 	
-if (num / 10)
-print_number_rec(num);
-while (num != 0)
-
-{
-	count++;
-	num /= 10;
-}
-return(count);
-}
