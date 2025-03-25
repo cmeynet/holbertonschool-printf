f5ebe270efb2c5ab8aaa94e17d4a392c6bd36134
Pulon Julien <pulonjulien@gmail.com>
2025-03-25 15:03:20 +0100

    add count in return of function print_number

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	_printf("Negative:[%d]\n", -7);
+	return (0);
+}
