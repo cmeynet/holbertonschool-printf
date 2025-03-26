9f842298565c771dc0f132ec50f5ab10460aa5bb
cmeynet <clemence.meynet14@gmail.com>
2025-03-26 12:40:23 +0100

    fix _printf add function to check the specifiers

--- _printf.c
+++ _printf.c
@@ -1,42 +1,41 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * _printf - main fonction printf
- * 
- * Return: 
+ *
+ * Return:
  */
 int _printf(const char *format, ...)
 {
-	print_t specifiers[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{NULL, NULL}
-	};
+        print_t specifiers[] = {
+                {"c", print_char},
+                {"s", print_string},
+                {NULL, NULL}
+        };
 
-	va_list args;
-	unsigned int i = 0, j;
-	/* const char *separator = "";*/
+        va_list args;
+        unsigned int i = 0, j;
+        /* const char *separator = "";*/
 
-	va_start(args, format);
+        va_start(args, format);
 
-	while (format && format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			j = 0;
-			while (specifiers[j].type != NULL)
-			{
-		
-				if (format[i] == *specifiers[j].type)
-					specifiers[j].f(args);
-				j++;
-			}
-		}
-		i++;
-	}
+        while (format && format[i] != '\0')
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        j = 0;
+                        while (specifiers[j].type != NULL)
+                        {
 
-	_putchar('\n');
-	va_end(args);
-	return (0);
+                                if (format[i] == *specifiers[j].type)
+                                        specifiers[j].f(args);
+                                j++;
+                        }
+                }
+                i++;
+        }
+
+        _putchar('\n');
+        va_end(args);
+        return (0);
 }
