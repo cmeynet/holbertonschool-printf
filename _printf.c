d5d8d3010c612aeb1ab96ac73a50ade23f73d3f3
cmeynet <clemence.meynet14@gmail.com>
2025-03-26 11:58:20 +0100

    fix _printf add function to check specifiers

--- _printf.c
+++ _printf.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _printf - main fonction printf
- * 
- * Return: 
+ * _printf - fonction that recodes the printf function
+ * @format: list of types of arguments
+ * passed to the function
+ *
+ * Return: the total number of characters printed
+ * (excluding the final \0)
  */
 int _printf(const char *format, ...)
 {
 	print_t specifiers[] = {
 		{"c", print_char},
 		{"s", print_string},
+		{"%", print_percent},
+		{"i", print_number},
+		{"d", print_number},
 		{NULL, NULL}
 	};
-
 	va_list args;
-	unsigned int i = 0, j;
+	unsigned int i = 0;
 	int count = 0;
+	int index; /* Find if the specifier exist */
 
 	va_start(args, format);
 	while (format && format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '%')
+			i++;
+			index = specifier_exist(specifiers, format[i]);
+			if (index != -1) /* Specifier found */
+				count += specifiers[index].f(args);
+			else  /* Specifier not found: display `%` followed by the character */
 			{
 				_putchar('%');
-				count++;
-				i += 2;
-				continue;
-			}
-			i++;
-			j = 0;
-			while (specifiers[j].type != NULL)
-			{
-				if (format[i] == *specifiers[j].type)
-				{
-					count += specifiers[j].f(args);
-					break;
-				}
-				j++;
+				_putchar(format[i]);
+				count += 2;
 			}
 		}
 		else
