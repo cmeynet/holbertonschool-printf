87a007831ab76ba21d0c1b3b9639dafb27706e5a
cmeynet <clemence.meynet14@gmail.com>
2025-03-26 12:52:01 +0100

    Create specifier_exist.c to check if specifier exist

---
+++ specifier_exist.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * specifier_exist - Function that checks
+ * if a specifier exists in an array
+ * @specifiers: array to browse
+ * @spec: desired character
+ *
+ * Return: i if the specifier is finded
+ * -1 if the specifier doesn't exist
+ */
+int specifier_exist(print_t specifiers[], char spec)
+{
+        int i;
+
+        for (i = 0; specifiers[i].type != NULL; i++)
+        {
+                if (specifiers[i].type[0] == spec)
+                {
+                        return (i);
+                }
+        }
+        return (-1);
+}
