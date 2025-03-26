cd092c0e61f5dc5a270b98208f6be584b2dac2f2
cmeynet <clemence.meynet14@gmail.com>
2025-03-26 12:50:08 +0100

    Create print_percent to check %

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_percent - Function that prints %
+ * @args: variable argument
+ *
+ * Return: number of character printed (1)
+ */
+int print_percent(va_list args)
+{
+        (void)args;
+        _putchar('%');
+        return (1);
+}
