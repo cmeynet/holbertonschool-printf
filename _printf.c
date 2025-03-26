e961e316d68c03afa8915c1da5a0e51f7ae5c7fa
cmeynet <clemence.meynet14@gmail.com>
2025-03-26 12:44:13 +0100

    fix _printf add function to check the specifiers

--- _printf.c
+++ _printf.c
@@ -1,41 +1,50 @@
 #include "main.h"
 /**
- * _printf - main fonction printf
+ * _printf - fonction that recodes the printf function
+ * @format: list of types of arguments
+ * passed to the function
  *
- * Return:
+ * Return: the total number of characters printed
+ * (excluding the final \0)
  */
 int _printf(const char *format, ...)
 {
-        print_t specifiers[] = {
-                {"c", print_char},
-                {"s", print_string},
-                {NULL, NULL}
-        };
+	print_t specifiers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"i", print_number},
+		{"d", print_number},
+		{NULL, NULL}
+	};
+	va_list args;
+	unsigned int i = 0;
+	int count = 0;
+	int index; /* Find if the specifier exist */
 
-        va_list args;
-        unsigned int i = 0, j;
-        /* const char *separator = "";*/
-
-        va_start(args, format);
-
-        while (format && format[i] != '\0')
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        j = 0;
-                        while (specifiers[j].type != NULL)
-                        {
-
-                                if (format[i] == *specifiers[j].type)
-                                        specifiers[j].f(args);
-                                j++;
-                        }
-                }
-                i++;
-        }
-
-        _putchar('\n');
-        va_end(args);
-        return (0);
+	va_start(args, format);
+	while (format && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			index = specifier_exist(specifiers, format[i]);
+			if (index != -1) /* Specifier found */
+				count += specifiers[index].f(args);
+			else  /* Specifier not found: display `%` followed by the character */
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
